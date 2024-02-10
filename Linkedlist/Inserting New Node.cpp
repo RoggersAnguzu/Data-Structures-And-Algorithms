b5e9c9d80123935cc699e80f4cae62b475093850
RoggersAnguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2024-02-10 12:23:21 +0300

    Add files via upload

---
+++ Linkedlist/Inserting New Node.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	node *next;
+};
+
+class linkedlist{
+	node *head, *tail;
+	public:
+		void createnode(int item){
+			node *newnode=new node;
+			newnode->data=item;
+			newnode->next=NULL;
+			
+			if(head==NULL){
+				head=newnode;
+				tail=newnode;
+			}
+			else{
+				tail->next=newnode;
+				tail=newnode;
+			}
+		}
+		
+		//inserting new node;
+		void insertnewnode(int item){
+			node *newnode=new node;
+			newnode->data=item;
+			newnode->next=NULL;
+			
+			int i,position;
+			cout<<"Enter the Position Where to insert the new node";
+			cin>>position;
+			
+			node *current=head;
+			node *previous;
+			
+			for(i=0;i<position;i++){
+				previous=current;
+				current=current->next;
+			}
+			if(current==head){
+				newnode->next=current;
+				head=newnode;
+			}
+			else if(current==tail){
+				tail->next=newnode;
+				tail=newnode;
+			}
+			else{
+				previous->next=newnode;
+				newnode->next=current;
+			}
+		}
+		
+		void display(){
+			node *temp=head;
+			while(temp!=NULL){
+				cout<<temp->data<<"<--->";
+				temp=temp->next;
+			}
+		}
+};
+
+int main(){
+	linkedlist usiu;
+	int i, n;
+	int item;
+	cout<<"How many Values are you Entering: ";
+	cin>>n;
+	cout<<"Enter the Values: ";
+	for(i=1;i<n;i++){
+		cin>>item;
+		usiu.createnode(item);
+	}
+	usiu.display();
+	cout<<"Enter the New Nodes to Insert \n";
+	cin>>item;
+	usiu.insertnewnode(item);
+	usiu.display();
+}
---
+++ Linkedlist/Linkedlist.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+	int data;
+	node *next;
+};
+
+class linkedlist{
+	node *head, *tail;
+	public:
+		void createnode(int item){
+			node *newnode=new node;
+			newnode->data=item;
+			newnode->next=NULL;
+			
+			if(head==NULL){
+				head=newnode;
+				tail=newnode;
+			}
+			else{
+				tail->next=newnode;
+				tail=newnode;
+			}
+		}
+		
+		void display(){
+			node *temp=head;
+			while(temp!=NULL){
+				cout<<temp->data<<"<--->";
+				temp=temp->next;
+			}
+		}
+};
+
+int main(){
+	linkedlist usiu;
+	int i, n;
+	int item;
+	cout<<"Enter the Values: ";
+	cin>>n;
+	cout<<"Enter the Values: ";
+	for(i=0;i<n;i++){
+		cin>>item;
+		usiu.createnode(item);
+	}
+	usiu.display();
+}
